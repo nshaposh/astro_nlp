2e83bbd0de622868cd87a1631aab08c406d1c156
Nikolai Shaposhnikov <nikolai.v.shaposhnikov@nasa.gov>
2018-04-05 12:48:37 -0400

    added plot_confusion_matrix

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 """ Utility functions for abstracts project"""
 
 import urllib.request as ul
+from operator import itemgetter
+import matplotlib.pyplot as plt
 
 
 def get_abstracts_txt(year='2017',month='01'):
@@ -76,3 +78,39 @@ def get_pubs_dict(
 #            print(p)
     
     return pubs_dict, "".join(all_abstracts)
+
+def plot_confusion_matrix(cm, classes,max_val = 300,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues,cb=False):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    cm_clip = copy.deepcopy(cm)
+    cm_clip[cm>max_val] = max_val 
+    plt.imshow(cm_clip, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    if cb: plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#        print("Normalized confusion matrix")
+#    else:
+#        print('Confusion matrix, without normalization')
+
+#    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
